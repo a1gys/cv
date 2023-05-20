66862a21af400c5c6b0f8bc379e80d83f7a38542
Algys <algysrakhmetov@gmail.com>
2023-05-21 01:27:50 +0600

    small changes

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,5 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+pub mod imagegray;
+pub mod error;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+use crate::imagegray::ImageGray;
+use crate::error::*;
